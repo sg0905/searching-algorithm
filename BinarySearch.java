4fadb85ac31d7e63c97e01a70cf5bd85447fd0d7
Shubham <SHUBHAMTHE9@GMAIL.COM>
2024-06-01 23:56:04 +0530

    Added binary search coded in java language

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,27 @@
 public class BinarySearch {
-    
+    public void run(int ar[], int element){
+        int len = ar.length;
+        int start = 0;
+        int end = len;
+        
+        while(start < end){
+            int mid = (start+end)/2;
+            if(ar[mid] == element){
+                System.out.println("Found the element at position " + (mid+1));
+                break;
+            }
+            else if( ar[mid] > element ){
+                end = mid-1;
+            }
+            else{
+                start = mid + 1; 
+            }
+        }
+    }
+    public static void main(String args[]){
+        int ar [] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int element = 60;
+        BinarySearch bs = new BinarySearch();
+        bs.run(ar, element);
+    }
 }
